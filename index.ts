69c31299d78ae8b1a13da37600232facd43a47c6
Robert Coe <chakrawc@gmail.com>
2021-06-20 16:34:41 -0700

    testing?

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
-// Sift is a small routing library that abstracts the details like registering
-// a fetch event listener and provides a simple function (serve) that has an
-// API to invoke a function for a specific path.
 import {
   json,
   serve,
   validateRequest,
 } from "https://deno.land/x/sift@0.3.2/mod.ts";
-// TweetNaCl is a cryptography library that we use to verify requests
-// from Discord.
 import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
 import { interactionsMap } from "./interactionsMap.ts";
-// import type { APIInteraction } from "https://esm.sh/discord-api-types@0.18.1/deno/v9";
 
 const PING = 1;
 const PONG = 1;
